dde45906aa11e660eb9eacc96043d3c4790cf116
ElreyB <elbelmonti@gmail.com>
2017-07-17 18:24:02 -0700

    refactor name and code

--- HasTwoDigits.cs
+++ HasTwoDigits.cs
@@ -1,11 +1,12 @@
 using System;
 
-class TwoDigits
+class HasTwoDigits
 {
   static void Main()
   {
     Console.WriteLine("Give me a whole number:");
     string stringUserNumber = Console.ReadLine();
-    int intUserNumber = 
+    int intUserNumber = int.Parse(stringUserNumber);
+    Console.WriteLine(intUserNumber >= 10);
   }
 }
