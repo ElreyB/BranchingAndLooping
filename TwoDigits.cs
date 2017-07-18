87a37f49066dd2b28686772fb481e707c7c2abb6
ElreyB <elbelmonti@gmail.com>
2017-07-17 17:24:18 -0700

    WIP

---
+++ TwoDigits.cs
@@ -0,0 +1,11 @@
+using System;
+
+class TwoDigits
+{
+  static void Main()
+  {
+    Console.WriteLine("Give me a whole number:");
+    string stringUserNumber = Console.ReadLine();
+    int intUserNumber = 
+  }
+}
